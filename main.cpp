3d5a004306f465b441516c21cf655f2740930e73
Miya Furikadze <50241874+cutemiya@users.noreply.github.com>
2022-12-20 22:44:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     list l;
 
+    l.addTop(1);
+    l.addTop(2);
     l.addTop(3);
-    l.addTop(123);
-    l.addTop(8);
-    l.addArc(123, l[2]);
-    l.addArc(123, l[0]);
+    l.addArc(2, l[2]);
+    l.addArc(2, l[0]);
 
     l.printArcs();
 
